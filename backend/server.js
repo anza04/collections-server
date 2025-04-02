7e5851e71bca463220c6fee7e684b1b71ce76bae
Federico Anzani <federico.anzani04@gmail.com>
2025-04-02 09:13:33 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/test', async(req, res)=>{
-    res.json({ message: 'Test succesfull, p2' });
+    res.json({ message: 'Test succesfull, p3' });
 });
 
 app.listen(3000, ()=>(console.log('App listening on port 3000')));
